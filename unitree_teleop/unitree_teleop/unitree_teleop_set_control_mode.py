19eb209f39890c74673312c038c9c872a356a040
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-05-19 14:40:42 +0900

    improve teleop

--- unitree_teleop/unitree_teleop/unitree_teleop_set_control_mode.py
+++ unitree_teleop/unitree_teleop/unitree_teleop_set_control_mode.py
@@ -1,13 +1,12 @@
-from unittest import result
 import rclpy
 
 from unitree_msgs.srv import SetControlMode
 
 import sys, select, termios, tty
 
-settings = termios.tcgetattr(sys.stdin)
+SETTINGS = termios.tcgetattr(sys.stdin)
 
-msg = """
+CONSOLE_MSG = """
 Set the Control Mode from Keyboard!
 ---------------------------
 0: Zero Torque Mode
@@ -25,58 +24,59 @@ Only the following mode tansitions are allowed. Otherwise, the request is denied
 4 -> 0
 
 CTRL-C to quit
+---------------------------
 """
 
 def getKey():
-	tty.setraw(sys.stdin.fileno())
-	select.select([sys.stdin], [], [], 0)
-	key = sys.stdin.readline(1)[0]
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
+    tty.setraw(sys.stdin.fileno())
+    select.select([sys.stdin], [], [], 0)
+    key = sys.stdin.read(1)
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, SETTINGS)
+    return key
 
 def request_to_str(mode):
-	return "request mode:\t %s " % (mode)
+    return "request mode:\t %s " % str(mode)
 
 def response_to_str(mode):
-	return "request mode:\t %s " % (mode)
-
-# def vels(speed,turn):
-# 	return "currently:\tspeed %s\tturn %s " % (speed,turn)
+    return "request mode:\t %s " % str(mode)
 
 
-def main(args=None):	
-	rclpy.init(args=args)
-	node = rclpy.create_node('unitree_set_control_mode')
-	cli = node.create_client(SetControlMode, 'set_control_mode')
+def main(args=None):
+    rclpy.init(args=args)
+    node = rclpy.create_node('unitree_set_control_mode')
+    cli = node.create_client(SetControlMode, 'unitree_controller/set_control_mode')
 
-	request = SetControlMode.Request()
-	request.control_mode = 0
-	status = 0
+    request = SetControlMode.Request()
+    request.control_mode = 0
+    while not cli.wait_for_service(timeout_sec=1.0):
+        node.get_logger().info('service SetControlMode not available, waiting again...')
 
-	print(msg)
-	while(1):
-		key = getKey()
-		if key in ['0', '1', '2', '3', '4']:
-			request.control_mode = int(key)
-			print(request_to_str(request.control_mode))
-			if (status == 14):
-				print(msg)
-			status = (status + 1) % 15
+    print(CONSOLE_MSG)
 
-			future = cli.call_async(request)
-			rclpy.spin_until_future_complete(node, future)
-			result = future.result()
-			node.get_logger().info('SetControlMode.success: ' % (result.success))
-			node.get_logger().info('SetControlMode.current_control_mode: ' % (result.current_control_mode))
+    while rclpy.ok():
+        key = getKey()
+        if key in ['0', '1', '2', '3', '4']:
+            request.control_mode = int(key)
+            print(request_to_str(request.control_mode))
 
-		else:
-			print('Choose the ControlMode from [0, 1, 2, 3, 4]')
+            future = cli.call_async(request)
+            rclpy.spin_until_future_complete(node, future)
+            response = future.result()
+            print(CONSOLE_MSG)
+            node.get_logger().info('SetControlMode.accept: %s' % str(response.accept))
+            node.get_logger().info('SetControlMode.current_control_mode: %s' % str(response.current_control_mode))
 
-		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+        elif key == '\x03':
+            shutdown = True
+            break
 
-	node.destroy_node()
-	rclpy.shutdown()
+        else:
+            print('Choose the ControlMode from [0, 1, 2, 3, 4]: The input key was ' + key[0])
 
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, SETTINGS)
+
+    node.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
